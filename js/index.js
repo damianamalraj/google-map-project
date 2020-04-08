9fe7d2ae0656528242a404bd01d4bffa9c5541fb
Damian Clitus Robert Amalraj <45610772+DamianClitus@users.noreply.github.com>
2020-04-08 19:59:12 +0200

    d2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function initMap() {
     var sydney = {
-        lat: 34.063380, 
-        lng: -118.358080
+      lat: 34.063380, 
+      lng: -118.358080
     };
     map = new google.maps.Map(document.getElementById('map'), {
       center: sydney,
@@ -9,4 +9,22 @@ function initMap() {
       mapTypeId: 'roadmap',
       
     });
-} 
+}
+
+function initMap() {
+  var myLatLng = {
+    lat: 34.063380, 
+    lng: -118.358080
+  };
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
