fcd3bcd9292103c7cd5fe23db792d26bb3b2312a
Damian Clitus Robert Amalraj <45610772+DamianClitus@users.noreply.github.com>
2020-04-10 03:05:43 +0200

    d4

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 
 window.onload = () => {
-  displayStores();
+  
+  
 }
 
 var map;
@@ -8,23 +9,64 @@ var markers = [];
 var infoWindow;
 
 function initMap() {
-    var sydney = {
-      lat: 34.063380, 
-      lng: -118.358080
-    };
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: sydney,
-      zoom: 11,
-      mapTypeId: 'roadmap',
-      
-    });
-    infoWindow = new google.maps.InfoWindow();
-    showStoresMarkers();
+  var sydney = {
+    lat: 34.063380, 
+    lng: -118.358080
+  };
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: sydney,
+    zoom: 11,
+    mapTypeId: 'roadmap',
+    
+  });
+  infoWindow = new google.maps.InfoWindow();
+
+  searchStores()
+    
 }
 
 
+function searchStores(){
+  var foundStores = [];
+  var zipCode = document.getElementById('zip-code-input').value;
+  console.log(zipCode);
+  if(zipCode){
+    for(var store of stores){
+      var postal = store['address']['postalCode'].substring(0, 5);
+      if(postal == zipCode){
+        foundStores.push(store);
+      }
+    }
+  } else {
+    foundStores = stores;
+  }
+  clearLocations()
+  displayStores(foundStores);
+  showStoresMarkers(foundStores);
+  setOnClickListener();
+}
 
-function displayStores(){
+function clearLocations() {
+  function clearLocations() {
+    infoWindow.close();
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+    markers.length = 0;
+
+  }
+}
+
+function setOnClickListener() {
+  var storeElements = document.querySelectorAll('.store-container');
+  storeElements.forEach(function (elem, index) {
+    elem.addEventListener('click', function(){
+      google.maps.event.trigger(markers[index], 'click');
+    })
+  })
+}
+
+function displayStores(stores){
   var storesHtml = '';
   for(var [index, store] of stores.entries()){
 
@@ -33,17 +75,19 @@ function displayStores(){
 
 
     storesHtml += `
-        <div class="store-container">
-        <div class="store-info-container">
-            <div class="store-address">
-                <span>${address[0]}</span> 
-                <span>${address[1]}</span> 
-                
-            </div>
-            <div class="store-phone-number">${phone}</div>
-        </div>
-        <div class="store-number-container">
-            <div class="store-number">${index+1}</div>
+      <div class="store-container">
+        <div class="store-container-background">
+          <div class="store-info-container">
+              <div class="store-address">
+                  <span>${address[0]}</span> 
+                  <span>${address[1]}</span> 
+                  
+              </div>
+              <div class="store-phone-number">${phone}</div>
+          </div>
+          <div class="store-number-container">
+              <div class="store-number">${index+1}</div>
+          </div>
         </div>
     </div>
     `
@@ -52,7 +96,7 @@ function displayStores(){
   }
 }
 
-function showStoresMarkers(){
+function showStoresMarkers(stores){
   var bounds = new google.maps.LatLngBounds();
   for(var [index, store] of stores.entries()){
 
@@ -65,15 +109,39 @@ function showStoresMarkers(){
 
     var name = store['name'];
     var address = store['addressLines'][0];
+
+    var openStatusText = store["openStatusText"];
+    var phoneNumber = store["phoneNumber"];
     bounds.extend(latlng);
-    createMarker(latlng, name, address, index+1);
+    createMarker(latlng, name, address, openStatusText, phoneNumber, index+1);
   }
 
   map.fitBounds(bounds);
 }
 
-function createMarker(latlng, name, address, index){
-  var html = "<b>" + name + "</b> <br/>" + address;
+function createMarker(latlng, name, address, openStatusText, phoneNumber, index){
+  var html = `
+    <div class="store-info-window">
+      <div class="store-info-name">
+        ${name}
+      </div>
+      <div class="store-info-status">
+        ${openStatusText}
+      </div>
+      <div class="store-info-address">
+        <div class="circle">
+          <i class="fas fa-location-arrow"></i>
+        </div>
+        ${address}
+      </div>
+      <div class="store-info-phone">
+        <div class="circle">
+          <i class="fas fa-phone-alt"></i>
+        </div>
+        ${phoneNumber}
+      </div>
+    </div>
+  `;
   var marker = new google.maps.Marker({
     map: map,
     position: latlng,
@@ -84,4 +152,5 @@ function createMarker(latlng, name, address, index){
     infoWindow.open(map, marker);
   });
   markers.push(marker);
+  
 }
